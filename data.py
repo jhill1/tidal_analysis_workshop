1e8f75712b723595769920b4b73f297844fc20c5
jh1889 <jon.hill@york.ac.uk>
2023-10-04 07:01:37 +0100

    adding data sources and outline of script

---
+++ data.py
@@ -0,0 +1,18 @@
+
+
+# wget data
+# munge dates (need function to do this; create module for common crap like this)
+# plot
+# plot subset
+# do same for other data
+# plot each station as sep line for same dates
+
+# analysis:
+#   - use 40 days
+#   - remove linear trend
+#   - plot
+#   - calculate Rayleigh
+#   - extract a single consist
+#   - plot and compare to real signal
+#   - extract multiple and compare
+#   - Do same for longer data period (e.g. year)
