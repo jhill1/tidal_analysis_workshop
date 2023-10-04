4ffc0e5081491514fef5520afaae64411ab427c5
jh1889 <jon.hill@york.ac.uk>
2023-10-04 08:03:13 +0100

    Loading and processing data

--- data.py
+++ data.py
@@ -1,5 +1,13 @@
 
+import pandas as pd
 
+Fort_Denison = pd.read_csv("h333.csv", header=None)
+Fort_Denison['Date'] = pd.to_datetime(dict(year=Fort_Denison[0], month=Fort_Denison[1], day=Fort_Denison[2], hour=Fort_Denison[3]))
+# col 0 is year, col 1 is month, col2 is day, col3 hour
+Fort_Denison = Fort_Denison.drop([0,1,2,3], axis = 1)
+Fort_Denison = Fort_Denison.rename(columns={4: "Tide"})
+Fort_Denison = Fort_Denison.set_index('Date')
+Fort_Denison = Fort_Denison.mask(Fort_Denison['Tide'] < -300)
 # wget data
 # munge dates (need function to do this; create module for common crap like this)
 # plot
