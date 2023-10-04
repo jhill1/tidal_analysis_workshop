7807afd5118335c2825434231539aa9ccc8399af
Jon Hill <jon.hill@york.ac.uk>
2023-10-04 13:12:59 +0100

    plotting

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
-
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
 
 Fort_Denison = pd.read_csv("h333.csv", header=None)
 Fort_Denison['Date'] = pd.to_datetime(dict(year=Fort_Denison[0], month=Fort_Denison[1], day=Fort_Denison[2], hour=Fort_Denison[3]))
@@ -8,6 +10,14 @@ Fort_Denison = Fort_Denison.drop([0,1,2,3], axis = 1)
 Fort_Denison = Fort_Denison.rename(columns={4: "Tide"})
 Fort_Denison = Fort_Denison.set_index('Date')
 Fort_Denison = Fort_Denison.mask(Fort_Denison['Tide'] < -300)
+
+fig_summary=plt.figure(figsize=(12.0,6),dpi=180)
+ax=fig_summary.add_subplot(111)
+tidal_measure = ax.plot(Fort_Denison['Tide'], color="blue", lw=2, label="Tide gauge", alpha=0.4)
+ax.set_xlabel("Date")
+ax.set_ylabel("Water height (mm)")
+ax.set_xlim([datetime.date(2020, 2, 1), datetime.date(2020, 3, 1)])
+
 # wget data
 # munge dates (need function to do this; create module for common crap like this)
 # plot
